57f76ba0aa4f5b1b16b9a5557958881bb18c2fa5
Deepak <d.kadarivel@relay42.com>
2021-02-16 23:43:48 +0100

    Deploy script update

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:deepakkadarivel/currency-converter.git main:gh-pages
+git push -f git@github.com:deepakkadarivel/currency-converter.git master:gh-pages
 cd -
